95a968d74e7c789421f66a8b721ba9039ae9e577
Ulises Mendoza <69097655+UlisesMendozaOlvera@users.noreply.github.com>
2022-09-15 16:36:28 -0500

    Actualizacion 15-sep

--- Parcial1/EjercicioServidorExpress/servidorExpress.js
+++ Parcial1/EjercicioServidorExpress/servidorExpress.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors= require('cors')
+const cors = require('cors')
 const app = express()
 
-app.use(cors({ origin: "http://localhost" }))
+app.use(cors())
 
 
 //app.use(express.static('Proyecto'));
@@ -15,7 +15,7 @@ app.post('/', (req, res) => {
     res.send('Hiciste post al server de express')
 })
 
-app.listen(8082, function () {
+app.listen(8085, function () {
 
     console.log('Servidor http correindo en el puerto 8082');
 })
