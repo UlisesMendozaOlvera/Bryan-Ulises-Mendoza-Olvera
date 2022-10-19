889f55b08c3148679cd0a97036b6445d7c9a5356
Ulises Mendoza <69097655+UlisesMendozaOlvera@users.noreply.github.com>
2022-10-18 20:30:18 -0500

    Actualizacion

---
+++ Parcial2/moduloesmEstatico/moduloesm.js
@@ -0,0 +1,9 @@
+export function pasarMayusculas(cadena) {
+    return cadena.toUpperCase()
+}
+export function quitarEspacios(cadena) {
+    return cadena.split(" ").join("");
+}
+export function obtenerLongitud(cadena) {
+    return cadena.length;
+}
